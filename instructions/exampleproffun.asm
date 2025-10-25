29c250378b1a99f77c12b0258e36815a745fe8d4
tempcracks <sernautmoka@gmail.com>
2025-10-25 17:38:30 +0000

    Update exampleproffun.asm

--- instructions/exampleproffun.asm
+++ instructions/exampleproffun.asm
@@ -26,9 +26,6 @@ professional_func:
     popl %ebp
     ret $8                    # Удаляем аргументы
 
-
-
-
   pushl %ebx, %esi, %edi - сохраняем callee-saved регистры (те, которые функция обязана сохранить по соглашению ABI)
 
   high memory address
@@ -45,3 +42,22 @@ professional_func:
   | saved ebx      |<- ebp-12
   -----------------
   |  local variables|<- ebp-12 = local variables
+
+
+function parameters
+RETURN value: for integer or pointer return types, its placed in RAX
+Parameters (in order):
+1st argument: RDI
+2nd argument: RSI
+3nd argument: RDX
+4th argument: RCX
+5th argument: R8
+6th argument: R9
+7th+ argument passed on the stack
+
+The Stack
+the stack pointer (RSP) must be 16-byte aligned before a call instruction
+the call instruction pushes the 8-byte return address,so upon entry to a function,
+RSP is RSP-8 breaking the alignment. the function prologue often adjusts it back
+(e.g. with a push rbp)
+
